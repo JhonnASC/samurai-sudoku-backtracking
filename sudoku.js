32bf829c62b1f1a37a89e40f5df00466cc5e6604
Joctox04 <99295799+Joctox04@users.noreply.github.com>
2023-03-28 20:46:39 -0600

    Sudoku terminado y con los botones

--- sudoku.js
+++ sudoku.js
@@ -8,53 +8,133 @@ function prueba (parametro1){
 }
 
 window.onload = function() {
-    crearCuadros();
-    crearCuadros2();
+    creatTopLeft();
+    creatTopRight();
+    creatCenter();
+    creatBottLeft();
+    creatBottRight();
 }
 
-function crearCuadros() {
+function creatTopLeft() {
     var tabla = document.createElement("table");
     var tbody = document.createElement("tbody");
 
     for (var i = 0; i < 9; i++) {//crea columnas
         var tr = document.createElement("tr");
         for (var j = 0; j < 9; j++) {//crea filas
+            if (j == 6 && i >= 6){ //para no crear cuadros de mas
+                break
+            }
+            var fila = document.createElement("td");//va creando las filas
+            var numero = document.createElement("input");//el cuadro para escribir el numero
+            numero.type = "number";//hace el cuadro de tipo numerico
+            fila.appendChild(numero);//agrega el cuadro a la fila
+            tr.appendChild(fila);//se agrega el elemento de la fila al tr, va desde el index 0 hasta el 9
+        }
+        tbody.appendChild(tr);//se agrega la fila a la tabla
+    }
+    tabla.appendChild(tbody);
+
+    document.getElementById("boardTopLeft").appendChild(tabla);
+}
+
+function creatTopRight() {
+    var tabla = document.createElement("table");
+    var tbody = document.createElement("tbody");
+
+    for (var i = 0; i < 9; i++) {//crea columnas
+        var tr = document.createElement("tr");
+        for (var j = 0; j < 9; j++) {//crea filas
+            if (i >= 6 && j <= 2) { //para no crear cuadros de mas
+                var fila = document.createElement("td");//va creando las filas
+                var numero = document.createElement("input");//el cuadro para escribir el numero
+                numero.type = "hidden"; //oculta los cuadros
+                fila.appendChild(numero);
+                tr.appendChild(fila);
+                
+            }else{
                 var fila = document.createElement("td");//va creando las filas
                 var numero = document.createElement("input");//el cuadro para escribir el numero
                 numero.type = "number";    
                 fila.appendChild(numero);
                 tr.appendChild(fila);
-            }
-            tbody.appendChild(tr);//se agrega la fila a la tabla
+            }     
+        }
+        tbody.appendChild(tr);//se agrega la fila a la tabla
     }
-    
     tabla.appendChild(tbody);
 
-    document.getElementById("boardTopLeft").appendChild(tabla);
     document.getElementById("boardTopRight").appendChild(tabla);
+}
+
+function creatCenter() {
+    var tabla = document.createElement("table");
+    var tbody = document.createElement("tbody");
+
+    for (var i = 0; i < 9; i++) {//crea columnas
+        var tr = document.createElement("tr");
+        for (var j = 0; j < 9; j++) {//crea filas
+            var fila = document.createElement("td");//va creando las filas
+            var numero = document.createElement("input");//el cuadro para escribir el numero
+            numero.type = "number";    
+            fila.appendChild(numero);
+            tr.appendChild(fila);
+        }
+        tbody.appendChild(tr);//se agrega la fila a la tabla
+    }
+    tabla.appendChild(tbody);
+
     document.getElementById("boardCenter").appendChild(tabla);
-    document.getElementById("boardBottLeft").appendChild(tabla);
-    document.getElementById("boardTopLeft").appendChild(tabla);
+}
 
-    }
-
-function crearCuadros2() {
+function creatBottLeft() {
     var tabla = document.createElement("table");
     var tbody = document.createElement("tbody");
 
     for (var i = 0; i < 9; i++) {//crea columnas
         var tr = document.createElement("tr");
         for (var j = 0; j < 9; j++) {//crea filas
+            if (j == 6 && i <= 2) { //para no crear cuadros de mas
+                break
+            }
+            var fila = document.createElement("td");//va creando las filas
+            var numero = document.createElement("input");//el cuadro para escribir el numero
+            numero.type = "number";    
+            fila.appendChild(numero);
+            tr.appendChild(fila);
+        }
+        tbody.appendChild(tr);//se agrega la fila a la tabla
+    }
+    tabla.appendChild(tbody);
+
+    document.getElementById("boardBottLeft").appendChild(tabla);
+}
+
+function creatBottRight() {
+    var tabla = document.createElement("table");
+    var tbody = document.createElement("tbody");
+
+    for (var i = 0; i < 9; i++) {//crea columnas
+        var tr = document.createElement("tr");
+        for (var j = 0; j < 9; j++) {//crea filas
+            if (i <= 2 && j <= 2) { //para no crear cuadros de mas
+                var fila = document.createElement("td");//va creando las filas
+                var numero = document.createElement("input");//el cuadro para escribir el numero
+                numero.type = "hidden";
+                fila.appendChild(numero);
+                tr.appendChild(fila);
+                
+            }else{
                 var fila = document.createElement("td");//va creando las filas
                 var numero = document.createElement("input");//el cuadro para escribir el numero
                 numero.type = "number";    
                 fila.appendChild(numero);
                 tr.appendChild(fila);
-            }
-            tbody.appendChild(tr);//se agrega la fila a la tabla
+            } 
+        }
+        tbody.appendChild(tr);//se agrega la fila a la tabla
     }
-    
     tabla.appendChild(tbody);
 
-    document.getElementById("boardTopRight").appendChild(tabla);
-    }
+    document.getElementById("boardBottRight").appendChild(tabla);
+}
