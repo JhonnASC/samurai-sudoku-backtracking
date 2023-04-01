bf435142c68fed5d9001e9b570e200c1e7f079a3
JoseJimenez01 <jg3633110@gmail.com>
2023-04-01 16:26:49 -0600

    Actualización de funciones
    Creadas las funciones que se usarán para resolver el sudoku supremo con el algoritmo A*.

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,96 @@
+/*****************************************************************************************************************************/
+/*                                                Global Structures                                                          */
+/*****************************************************************************************************************************/
+openList = [];  //For all the posible values of each node.
+closeList = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]; //For all the better values of each node.    ------hacer una matriz con la lista cerrada e ir actualizandola con los mejores valores para cada casilla.
+
+matSupIzq = [
+    [0, 0, 0, 5, 0, 0, 4, 0, 0],
+    [8, 5, 0, 0, 3, 0, 0, 0, 0],
+    [7, 0, 3, 0, 0, 0, 6, 0, 0],
+    [0, 0, 0, 8, 0, 7, 5, 0, 0],
+    [0, 0, 8, 0, 0, 0, 0, 7, 0],
+    [4, 2, 0, 0, 0, 0, 0, 9, 8],
+    [0, 0, 0, 0, 0, 0, 0, 0, 2],
+    [2, 0, 0, 9, 6, 5, 0, 4, 0],
+    [0, 0, 0, 0, 0, 3, 0, 6, 5],
+];
+
+
+matCentral = [
+    [0, 0, 2, 0, 0, 0, 0, 0, 0],
+    [0, 4, 0, 5, 0, 0, 8, 0, 6],
+    [0, 6, 5, 4, 3, 0, 0, 7, 9],
+    [4, 7, 0, 0, 0, 0, 0, 3, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 6, 0, 0, 0],
+    [0, 3, 9, 0, 0, 0, 0, 5, 0],
+    [0, 0, 1, 0, 0, 0, 0, 8, 7],
+    [5, 8, 4, 6, 0, 3, 0, 9, 0],
+];
+
+/**
+ * It validates if the values is in the column.
+*/
+function validateColumnValues(row, column, value){
+
+}
+
+/**
+ * It validates if the values is in the row.
+*/
+function validateRowValues(row, column, value){
+
+}
+
+/**
+ * It validates if the values is in the grid.
+*/
+function validateGridValues(row, column, value){
+
+}
+
+/**
+ * It update the values of the open list.
+*/
+function updateOpenL(){
+
+}
+
+/**
+ * It update the values of the close list.
+*/
+function updateCloseL(row, column, value){
+
+}
+
+/**
+ * It calls the fuctions that evaluates all the posible values of a especific node.
+*/
+function nodeEvaluation(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * Here we gonna put the information of the function.
